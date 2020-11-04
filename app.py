6fe534924430ad21046ae731edf3b90c862f15b8
gautami-8 <63532213+gautami-8@users.noreply.github.com>
2020-11-04 17:03:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ cdf = by_country.nlargest(n, 'Confirmed')[['Confirmed']]
 ## create a function that will return the updated data frame, cdf
 def find_top_confirmed(n = 15):
   import pandas as pd
-  corona_df = pd.read_csv('dataset.csv')
+  corona_df = pd.read_csv('dataset-1.csv')
   by_country = corona_df.groupby('Country_Region').sum()[['Confirmed', 'Deaths', 'Recovered', 'Active']]
   cdf = by_country.nlargest(n, 'Confirmed')[['Confirmed']]
   return cdf
